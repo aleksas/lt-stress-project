b457d9e5cb134373069863df40258db0204315fc
Aleksas Pielikis <ant.kampo@gmail.com>
2020-01-19 16:46:31 +0200

    Harmonizing output formats from different stressors

--- analyze_data.py
+++ analyze_data.py
@@ -1,11 +1,13 @@
 
 from os.path import isfile
 from zipfile import ZipFile
+import re
 import sqlite3
 import vdu_nlp_services.soap_stressor
 import vdu_nlp_services.morphological_analyzer
 from vdu_nlp_services import *
 from phonology_engine import PhonologyEngine
+from re_map import Processor
 
 '''for bi, annotated in enumerate(block.get_annotated()):
     elif isinstance(annotated, AnnotatedWord):
@@ -61,6 +63,8 @@ exceptions = [
     }
 ]
 
+_except_stress_pattern = re.compile('[^~`^]')
+
 def create_tables(cursor):
     queries = [
         '''CREATE TABLE IF NOT EXISTS "stress_text_cache" (
@@ -81,11 +85,48 @@ def load_cache(cursor):
     cursor.execute('SELECT `hash`, `text` FROM `stress_text_cache`')
     for h, out in cursor:
         result = {'out': out, 'Info': None, 'Klaida': None}
-        soap_stressor._stress_text_cache[h] = result
+        vdu_nlp_services.soap_stressor._stress_text_cache[h] = result
 
     cursor.execute('SELECT `hash`, `text` FROM `morphology_cache`')
     for h, text in cursor:
-        morphological_analyzer._morphology_cache[h] = text
+        vdu_nlp_services.morphological_analyzer._morphology_cache[h] = text
+
+def stress_text_liepa(pe, block):
+    liepa_processed_data = pe.process(block)
+    for word_details, _, _, _ in liepa_processed_data:
+        for word_detail in word_details:
+            span = word_detail['span_source']
+            is_stressed = len(_except_stress_pattern.sub('', word_detail['ascii_stressed_word'])) > 0
+            if is_stressed and not word_detail['normalized']:
+                yield word_detail['ascii_stressed_word'], span 
+
+def compare_replacements(text, replacements_maps):
+    comparison_replacements = {}
+    has_inequalities = False
+    keys = set([])
+    for replacements in replacements_maps:
+        keys = keys.union(set(replacements.keys()))
+    
+    for k in keys:
+        equal = True
+        value = None
+        for replacements in replacements_maps:
+            if k not in replacements:
+                equal = False
+                break
+            if not value:
+                value = replacements[k]
+            if value != replacements[k]:
+                equal = False
+                break
+
+        if equal:
+            comparison_replacements[k] = value
+        else:
+            has_inequalities = True
+            comparison_replacements[k] = '|| ' + ' <> '.join([(replacements[k] if k in replacements else ' ') for replacements in replacements_maps ]) + ' ||'
+    
+    return comparison_replacements, has_inequalities
 
 if __name__ == "__main__":
     '''strings = [
@@ -129,21 +170,21 @@ if __name__ == "__main__":
     create_tables(cursor)
 
     def set_stress_text_cache(h, result):
-        soap_stressor._stress_text_cache[h] = result
+        vdu_nlp_services.soap_stressor._stress_text_cache[h] = result
         try:
             cache_cursor.execute("INSERT INTO stress_text_cache (hash, text) VALUES (?,?)", (h, result['out']))
         except Exception as e:
             print(e)
 
     def set_morphology_cache(h, text):
-        morphological_analyzer._morphology_cache[h] = text
+        vdu_nlp_services.morphological_analyzer._morphology_cache[h] = text
         try:
             cache_cursor.execute("INSERT INTO morphology_cache (hash, text) VALUES (?,?)", (h, text))
         except Exception as e:
             print(e)
 
-    soap_stressor.set_stress_text_cache = set_stress_text_cache
-    morphological_analyzer.set_morphology_cache = set_morphology_cache
+    vdu_nlp_services.soap_stressor.set_stress_text_cache = set_stress_text_cache
+    vdu_nlp_services.morphological_analyzer.set_morphology_cache = set_morphology_cache
 
     load_cache(cursor)
 
@@ -156,6 +197,10 @@ if __name__ == "__main__":
 
     cursor.execute('SELECT article_id, `index`, block, url FROM article_blocks JOIN articles ON article_id = id WHERE article_id >= 4838')
 
+    pe = PhonologyEngine()
+    letter_pattern = u'A-ZĄ-Ža-zą-ž'
+    strip_acc = lambda x: re.sub(r'[\^~`]', '', x)
+
     for i, (article_id, index, block, url) in enumerate(cursor):
         if not block:
             continue
@@ -164,12 +209,26 @@ if __name__ == "__main__":
             conn.commit()
 
         exc_ = [e for e in exceptions if article_id in e['article_id']]
-        fused_replacements, augmented_elements = fused_stress_replacents(block, exc_)
-        fused_stressed_text = rebuild_text(augmented_elements, fused_replacements)
-        stressed_text = stress_text(block)
-        localizations = localize_stressed_text(stressed_text, augmented_elements)
+        fused_replacements, augmented_elements = fused_stress_replacements(block, exc_)
+        fused_stress_text, fused_stress_mappings = rebuild_text(augmented_elements, fused_replacements)
+        fused_stress_results = [(fused_stress_text[stressed_span[0]:stressed_span[1]], source_span) for source_span, stressed_span in fused_stress_mappings]
 
-        comparison_replacements, has_inequalities = compare_replacements([fused_replacements, localizations])
+        stressed_text = stress_text(block)
+        with Processor(stressed_text) as processor:
+            pattern =  r'([' + letter_pattern + r']+[\^~`][' + letter_pattern + ']*)'
+            replacement_map = { 1: strip_acc }
+            processor.process(pattern, replacement_map)
+
+            processor.swap()
+
+        stressed_results = [(stressed_text[stressed_span[0]:stressed_span[1]], source_span) for source_span, stressed_span in processor.span_map]
+
+        if processor.text != block:
+            raise Exception()
+
+        liepa_results = list(stress_text_liepa(pe, block))
+
+        comparison_replacements, has_inequalities = compare_replacements(block, [fused_stress_results, stressed_results, liepa_results])
 
         print (article_id, index)
         if has_inequalities:
